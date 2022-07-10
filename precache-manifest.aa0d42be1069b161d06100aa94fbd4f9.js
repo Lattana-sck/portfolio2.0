e218715b2bc7aafccc1dd172726b2a109f9b9cba
lattana <lattana.soucksengphet@epitech.eu>
2022-07-10 13:13:49 +0200

    Updates

---
+++ precache-manifest.aa0d42be1069b161d06100aa94fbd4f9.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd514249237636a4b0b8",
+    "url": "/portfolio2.0/static/css/main.b2e4c4fa.chunk.css"
+  },
+  {
+    "revision": "bd514249237636a4b0b8",
+    "url": "/portfolio2.0/static/js/main.bd514249.chunk.js"
+  },
+  {
+    "revision": "9bf32f55384023627f7f",
+    "url": "/portfolio2.0/static/js/1.9bf32f55.chunk.js"
+  },
+  {
+    "revision": "4a8b6bf3c0b4a61ac0f1",
+    "url": "/portfolio2.0/static/css/2.2a640b30.chunk.css"
+  },
+  {
+    "revision": "4a8b6bf3c0b4a61ac0f1",
+    "url": "/portfolio2.0/static/js/2.4a8b6bf3.chunk.js"
+  },
+  {
+    "revision": "586746817f6af8df284e",
+    "url": "/portfolio2.0/static/js/runtime~main.58674681.js"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/portfolio2.0/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "c26c05c183cf245dccb32761fe249ff7",
+    "url": "/portfolio2.0/static/media/github.c26c05c1.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/portfolio2.0/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/portfolio2.0/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/portfolio2.0/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/portfolio2.0/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/portfolio2.0/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "a0a13e950a000c80fe00a4472cd56dd6",
+    "url": "/portfolio2.0/static/media/previouslyon.a0a13e95.png"
+  },
+  {
+    "revision": "557edd96fe0d5ccf05487abe7ace4c22",
+    "url": "/portfolio2.0/static/media/snapchat.557edd96.png"
+  },
+  {
+    "revision": "3a3032932cdeb98b69240d6ca4152e4e",
+    "url": "/portfolio2.0/static/media/munchies.3a303293.png"
+  },
+  {
+    "revision": "434125935e2b1ef4ece53ffaa2bcb079",
+    "url": "/portfolio2.0/static/media/django.43412593.png"
+  },
+  {
+    "revision": "dd4566aaac0d9c3d729e9ad650239ed4",
+    "url": "/portfolio2.0/static/media/puissance4.dd4566aa.png"
+  },
+  {
+    "revision": "3f7e2df67cc461c37e7d5d57ce026523",
+    "url": "/portfolio2.0/static/media/carlab.3f7e2df6.png"
+  },
+  {
+    "revision": "29bacc06bad1049ed5b4205a35e6999b",
+    "url": "/portfolio2.0/static/media/cryptozombie.29bacc06.jpg"
+  },
+  {
+    "revision": "f8fac7c3437c66bf59f269bc1ecffec9",
+    "url": "/portfolio2.0/static/media/opensea.f8fac7c3.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/portfolio2.0/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/portfolio2.0/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio2.0/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/portfolio2.0/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/portfolio2.0/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/portfolio2.0/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/portfolio2.0/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/portfolio2.0/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolio2.0/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolio2.0/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portfolio2.0/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/portfolio2.0/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/portfolio2.0/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "be2a5070e2c8a4d9fbdc6ca6e2edb399",
+    "url": "/portfolio2.0/index.html"
+  }
+];
